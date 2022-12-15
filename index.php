2bef9e7e6cb444057c5f5ad34b72fef0c051f78a
Flavien Mayet <flavienmayet@macbook-pro-de-flavien.home>
2022-12-15 16:13:55 +0100

    Add includes header and footer

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-phpinfo();
+$page=[
+    "title" => "Track Calorie - Accueil"
+];
+
+include_once('includes/header.php');
+
+?>
+
+    <div class="container text-center">
+    <div class="row">
+        <h1>Salut</h1>
+        <div class="col-sm-12 col-md-4 col-lg-6">
+        Column
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-6">
+        Column
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-12">
+        Column
+        </div>
+    </div>
+    </div>
+
+<?php include_once('includes/footer.php'); ?>
